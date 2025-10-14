e4ef025b2038f4eb561134a8badb80eea3eda59e
Davi Costa <davi.costa@movedot.com>
2025-10-13 22:20:05 -0300

    created code smells

--- src/index.js
+++ src/index.js
@@ -1,6 +1,63 @@
+// Unused variable - code smell
+var unusedVariable = 'This is never used';
+
 function soma(a, b) {
+  // Console.log in production code - code smell
+  console.log('Calculando soma...');
   return a + b;
 }
 
-module.exports = soma;
+// Function with duplicated code
+function subtracao(a, b) {
+  console.log('Calculando subtracao...');
+  return a - b;
+}
 
+// Function with too many parameters and complex logic - code smell
+function calculoComplexo(x, y, z, w, v, u) {
+  if (x > 0) {
+    if (y > 0) {
+      if (z > 0) {
+        if (w > 0) {
+          return x + y + z + w;
+        }
+      }
+    }
+  }
+  return 0;
+}
+
+// Function with magic numbers - code smell
+function calcularDesconto(valor) {
+  if (valor > 100) {
+    return valor * 0.15; // Magic number
+  } else if (valor > 50) {
+    return valor * 0.1; // Magic number
+  }
+  return valor * 0.05; // Magic number
+}
+
+// Empty catch block - bug risk
+function funcaoComErro() {
+  try {
+    throw new Error('Erro proposital');
+  } catch (e) {
+    // Empty catch block - SonarQube will flag this
+  }
+}
+
+// Commented out code - code smell
+// function oldFunction() {
+//   return "This should be removed";
+// }
+
+// Function with == instead of === - code smell
+function comparacaoFraca(a, b) {
+  if (a == b) {
+    // Should use ===
+    return true;
+  }
+  return false;
+}
+
+module.exports = soma;
