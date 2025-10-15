102d1c049f2266f681b31cb406b78ea8095febc7
Davi Costa <davi.costa@movedot.com>
2025-10-15 09:54:05 -0300

    Revert "teste sonar"
    This reverts commit a1210f5fb00890db577bf41663988ef08f1ad7ad.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+// Unused variable - code smell
+var unusedVariable = 'This is never used';
+
 function soma(a, b) {
   // Console.log in production code - code smell
   console.log('Calculando soma...');
