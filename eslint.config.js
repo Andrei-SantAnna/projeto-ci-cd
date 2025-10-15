0984a5d6e1cb937133b8c5287bd6779b78703a39
Eric mnz <48776197+mi1048@users.noreply.github.com>
2025-10-15 10:46:30 -0300

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,9 @@
 export default [
   {
     files: ["*.js"],
-    languageOptions: {
-      ecmaVersion: 2024,
-      sourceType: "module",
-    },
     rules: {
-      "no-console": "warn",
-      "semi": ["error", "always"],
+      "no-console": "error",   // erro se usar console.log
+      "semi": ["error", "always"], // erro se faltar ;
     },
   },
 ];
