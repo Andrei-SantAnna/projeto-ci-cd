36445eb91b15a334f8bea24e178367d2d24c982a
Eric mnz <48776197+mi1048@users.noreply.github.com>
2025-10-15 10:00:14 -0300

    Create teste.js

---
+++ teste.js
@@ -0,0 +1,68 @@
+// Unused variable - code smell
+var unusedVariable = 'This is never used';
+
+function soma(a, b) {
+  // Console.log in production code - code smell
+  console.log('Calculando soma...');
+  return a + b;
+}
+
+function exemplo() {
+    let x = 10; // CodeQL detecta variável não usada
+    console.log("Hello world");
+}
+
+// Function with duplicated code
+function subtracao(a, b) {
+  console.log('Calculando subtracao...');
+  return a - b;
+}
+
+// Function with too many parameters and complex logic - code smell
+function calculoComplexo(x, y, z, w, v, u) {
+  if (x > 0) {
+    if (y > 0) {
+      if (z > 0) {
+        if (w > 0) {
+          return x + y + z + w;
+        }
+      }
+    }
+  }
+  return 0;
+}
+
+// Function with magic numbers - code smell
+function calcularDesconto(valor) {
+  if (valor > 100) {
+    return valor * 0.15; // Magic number
+  } else if (valor > 50) {
+    return valor * 0.1; // Magic number
+  }
+  return valor * 0.05; // Magic number
+}
+
+// Empty catch block - bug risk
+function funcaoComErro() {
+  try {
+    throw new Error('Erro proposital');
+  } catch (e) {
+    // Empty catch block - SonarQube will flag this
+  }
+}
+
+// Commented out code - code smell
+// function oldFunction() {
+//   return "This should be removed";
+// }
+
+// Function with == instead of === - code smell
+function comparacaoFraca(a, b) {
+  if (a == b) {
+    // Should use ===
+    return true;
+  }
+  return false;
+}
+
+module.exports = soma;
